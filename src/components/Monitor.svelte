f582124341253ecf5aaef3355819e2658870cd60
Jan-Luca Klees <email@janlucaklees.de>
2025-06-12 22:15:31 +0200

    Added empty screen for when no cameras are available
    When there are no cameras to receive feeds from, the screen used to be
    just blank - which I don't consider good ux. I want my software to
    communicate with the user.
    
    So in this commit I added some cute image to show up as well as a
    message telling you that there are no cameras available and what to do
    about that.

--- src/components/Monitor.svelte
+++ src/components/Monitor.svelte
@@ -8,6 +8,18 @@
 <div class="main relative mx-auto flex flex-col px-4 py-4">
 	{#each cameras as peer (peer.uuid)}
 		<Receiver {peer} />
+	{:else}
+		<div class="flex flex-col items-center h-full justify-center">
+			<img
+				src="/yuzukam_hiding.png"
+				class="w-auto h-56"
+				alt="A baby hiding their face, pretending not to be there."
+			/>
+			<div class="text-2xl -mt-4 mb-6">No babys here...</div>
+			<div class="bg-yellow-200 rounded-xl px-4 py-3 text-center">
+				Open this web page on another device and use it to keep an eye on your little one.
+			</div>
+		</div>
 	{/each}
 </div>
 
