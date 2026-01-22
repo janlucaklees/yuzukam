91cc294321178d66fa5445a0b24a15eec4fef9e1
Jan-Luca Klees <email@janlucaklees.de>
2026-01-22 21:27:23 +0100

    Improved settings menu
    Made the settings menu more prominent by making it a full fledged
    off-canvas.
    
    No animation for now sadly as there is no easy way for me to do that in
    tailwind.

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -23,17 +23,28 @@
 	});
 </script>
 
-<div class="fixed top-0 right-0 flex">
+{#if isMenuOpen}
+	<div class="fixed inset-0 h-screen w-screen bg-yellow-50 opacity-45"></div>
+{/if}
+
+<div class="fixed top-0 right-0">
 	<button
 		type="button"
 		onclick={() => (isMenuOpen = !isMenuOpen)}
-		class="flex h-14 w-14 flex-none cursor-pointer items-center justify-center rounded-bl-full bg-yellow-200 pb-3 pl-3"
+		class="absolute -left-14 flex h-14 w-14 flex-none cursor-pointer items-center justify-center rounded-bl-full bg-yellow-200 pb-3 pl-3 shadow-2xl shadow-gray-900"
 	>
 		<SettingsOutline />
 	</button>
+	<div class="absolute h-14 w-14 flex-none rounded-br-full bg-yellow-200"></div>
 
 	{#if isMenuOpen}
-		<div class="flex min-h-14 min-w-0 shrink flex-col gap-4 rounded-bl-lg bg-gray-300 px-4 py-4">
+		<div
+			class="flex h-screen min-w-0 shrink flex-col gap-4 rounded-bl-lg bg-gray-300 px-4 py-4 shadow-2xl shadow-gray-900"
+		>
+			<div class="mb-2 flex items-center justify-between">
+				<div class="pl-14 text-2xl">Settings</div>
+			</div>
+
 			<LabeledSelectInput
 				label="Mode:"
 				labelClass="min-w-28"
